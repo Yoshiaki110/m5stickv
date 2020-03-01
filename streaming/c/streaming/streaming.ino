b78c7146967ef96288b41db963ed02d8da5c7e1b
Yoshiaki Ito <yoshiaki110@gmail.com>
2020-03-01 16:58:15 +0900

    status display

--- streaming/c/streaming/streaming.ino
+++ streaming/c/streaming/streaming.ino
@@ -7,11 +7,41 @@
 
 const char* ssid = SSID;
 const char* password = PASS;
+String msg[4];
  
 WebServer server(80);
 uint8_t *buff;
 static const int RX_BUF_SIZE = 100000;
 
+void disp(String s1, String s2, String s3, String s4) {
+  if (s1 != "") {
+    msg[0] = s1;
+  }
+  if (s2 != "") {
+    msg[1] = s2;
+  }
+  if (s3 != "") {
+    msg[2] = s3;
+  }
+  if (s4 != "") {
+    msg[3] = s4;
+  }
+  M5.Lcd.fillScreen(TFT_BLACK);
+  M5.Lcd.setTextFont(2);      // 16ピクセル
+  M5.Lcd.setCursor(0, 0);     // x(右), y(下)
+  M5.Lcd.print(msg[0]);
+
+  M5.Lcd.setTextFont(1);      // 8ピクセル
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.print(msg[1]);
+  
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.print(msg[2]);
+  
+  M5.Lcd.setTextFont(2);      // 16ピクセル
+  M5.Lcd.setCursor(0, 60);     // x(右), y(下)
+  M5.Lcd.print(msg[3]);
+}
 
 bool transfer(WiFiClient client) {
   Serial.println("transfer start");
@@ -31,18 +61,18 @@ bool transfer(WiFiClient client) {
   int tLen = pLen;
   while (1) {
     len = Serial2.readBytes(buff, tLen);
-    if (len != tLen) {
-      Serial.print("transfer get picture retry ");
-      Serial.print(len);
-      Serial.print("-");
-      Serial.println(tLen);
-      tLen -= len;
-    } else if (len == 0) {
+    if (len == 0) {
       Serial.print("transfer get picture error ");
       Serial.print(len);
       Serial.print("-");
       Serial.println(tLen);
       return false;
+    } else if (len != tLen) {
+      Serial.print("transfer get picture retry ");
+      Serial.print(len);
+      Serial.print("-");
+      Serial.println(tLen);
+      tLen -= len;
     } else {
       break;
     }
@@ -64,15 +94,18 @@ bool transfer(WiFiClient client) {
 }
 
 void sendPic() {
+  disp("", "", "", "/pic");
   WiFiClient client = server.client();
   Serial.println("sendPic start");
 
   transfer(client);
 
   Serial.println("sendPic end");
+  disp("", "", "", "/pic end");
 }
 
 void streamPic() {
+  disp("", "", "", "/stream");
   Serial.println("streamPic start");
   WiFiClient client = server.client();
   String response = "HTTP/1.1 200 OK\r\n";
@@ -97,19 +130,21 @@ void streamPic() {
     }
   }
   Serial.println("streamPic end");
+  disp("", "", "", "/stream end");
 }
 
 void setup() {
-  pinMode(10, OUTPUT);
-  
   M5.begin();
-  M5.Axp.ScreenBreath(0);
-  
+//  M5.Axp.ScreenBreath(0);   // スクリーンの輝度を制御
+  M5.begin();
+  M5.Lcd.setRotation(1);
+    
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, 32, 33);
  
   buff = (uint8_t *) malloc(sizeof(uint8_t) * RX_BUF_SIZE);
  
+  disp(" AP : " +  String(ssid), "", "", "STATUS : Connecting...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -131,6 +166,7 @@ void setup() {
   Serial.print("  http://");
   Serial.print(WiFi.localIP());
   Serial.println("/stream");
+  disp("", " http://" + WiFi.localIP().toString() + "/pic", " http://" + WiFi.localIP().toString() + "/stream", "STATUS : Connected");
  
   if (MDNS.begin("esp32")) {
     Serial.println("MDNS responder started");
