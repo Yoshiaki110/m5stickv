fbaae88e11aa71ebc74aed70574697f2c0520ca5
ito <ito@C97440.citswd.canon-mj.co.jp>
2020-04-21 15:09:28 +0900

    modify servo.py

--- grove/servo.py
+++ grove/servo.py
@@ -1,10 +1,21 @@
 // https://hellobreak.net/m5stickv-pwm-10-9/
 
+'''
+MG996Rなど一般的なサーボ
+・周期：20[ms]、周波数：50[Hz]
+・パルス幅：0.5～2.4[ms]
+・Duty：2.5～12[%](パルス幅/周期×100)
+
+'''
 from machine import Timer,PWM
 import lcd
 import image
 import time
 import sys
+
+FREQ = 20
+PULSE_MIN = 0.5
+PULSE_MAX = 2.4
 
 lcd.init()
 img = image.Image()
@@ -12,6 +23,14 @@ img = image.Image()
 tim = Timer(Timer.TIMER0, Timer.CHANNEL0, mode=Timer.MODE_PWM)
 ch = PWM(tim, freq=50, duty=2.5, pin=34)
 arg = {-90:2.5, 0:7.25, 90:12}	# arg:duty
+
+def setAngle(ang):
+    diff = PULSE_MAX - PULSE_MIN
+    pich = diff / 180
+    pulse = ang * pich + PULSE_MIN
+    duty = pulse / FREQ * 100
+    print(duty)
+    ch.duty(duty)
 
 try:
     while True:
